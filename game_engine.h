a3b63fc37caa44c79696d6f970d2e0fe47f735f3
maxborglowe <62751158+maxborglowe@users.noreply.github.com>
2020-05-19 16:17:12 +0200

    Update game_engine.h

--- game_engine.h
+++ game_engine.h
@@ -20,9 +20,9 @@
 #define BTN_R PA3
 #define BTN_SEL PA4
 
-
 uint8_t DN = 1, UP = 2, L = 4, R = 8, SEL = 16;
-/*definar lite grejer som kanske används*/
+
+/** definar lite grejer som kanske används */
 #define Rows 10
 #define Cols 10
 
@@ -35,20 +35,24 @@ public int playerTwoBoard[][];
 public int playerTwoShootBoard[][]
 
 	int winner = 0;
-	int colSel = 0;
-	int rowSel = 0;
+	public int colSel = 0;
+	public int rowSel = 0;
 	int playerOneHit = 0;
 	int playerTwoHit = 0;
+	public bool toggle;
+	public bool setup;
+	public int shipType = 0;
+	public int turn = 0;
+	
 	bool noWinner = true;
-
-/* Init. knappar */
+	
 void buttonInit()
 {
 	DDR_BUTTONS = 0x00; /* Set DDR of I/O bus A to INPUT */
 }
 
 
-
+/** Polls the five pushbuttons */
 void buttonPoll(void)
 {
 	switch (PIN_BUTTONS)
@@ -63,255 +67,190 @@ void buttonPoll(void)
 		
 		case BTN_SEL: return SEL;
 	}
-} 
-
+}
 
 void selectGameType(uint8_t game_type){
 	if (game_type == 0)
 	{
 		initTwoPlayerGame();
 	}
-	else{
-		initFreeForAll();
-	}
+// 	else{
+// 		initFreeForAll();
+// 	}
 }
 
 
-/*Board ska vara 10x10 kommer behöva 2st
-context_save och Context_restore för att byta plan*/
+/** Initialize game boards.
+	player...Board = an overview of each player's own placement of ships on the board. 1 = ship placed, 0 = no ship placed
+	player...shootBoard = represents each player's hit board = overview of which parts of the board that the player has shot at.  */
 void initTwoPlayerGame(void){
-	/* spelare 1:s gamebaord, 2D matris*/
-	public int playerOneBoard[10][10];
-	/* spelare 1:s skjutbana dvs var han/hon skjuter */
-	public int playerOneShootBoard[10][10];
-	/* samma för spealre 2*/
-	public int playerTwoBoard[10][10];
-	public int playerTwoShootBoard[10][10]
-	
+	playerOneBoard[Rows][Cols];
+	playerOneShootBoard[Rows][Cols];
+
+	playerTwoBoard[Rows][Cols];
+	playerTwoShootBoard[Rows][Cols];
 }
-void placeShips{
-	/* shiptype är en int mellan 0-5 där siffran bestämmer vilket skäpp (längd)  */
-	int shipType = 0;
-	/* starta länst uppe till vänster i matrisen dvs 0,0 */
-	public int cols = 0;
-	public int rows = 0;
-	/* ska placera 5 skepp dvs numShips*/
-	for(i = 0; i < NumShips; i++){
-	/* stannar i while loopen så länge man inte trycker select så man kan använda alla andra knappar */
+
+/** Waits for player to press the select button */
+void waitForSelect(void){
+	
 	while (buttonPoll != SEL)
 	{
-		/* ser till att man håller sig inom matrisen, gåt till 0,0 om man går utanför*/
-		if (cols <= 10 && cols >=0 && rows <= 10 && rows >= 0)
-		{
-			
-			/* navigering inom matrisen som bygger på button_poll*/
-			if (buttonPoll == DN) {
-			rows++;
-			}
-			if else(buttonPoll == UP) {
-			rows--;
-			}
-			if else(buttonPoll == L){
-			cols--;
-			}
-			if else(buttonPoll == R){
-			cols++;
-			}	
-
-
+		if(buttonPoll == L) {
+			colSel = (colSel - 1)%Cols;
+			drawCursor();					//drawCursor() is placed in the if-satsers so as not to draw unless a button is pushed :)
 		}
-		else
-		{
-			rows = 0;
-			cols = 0;
+		else if(buttonPoll == R) {
+			colSel = (colSel + 1)%Cols;
+			drawCursor();
+		}
+		if (buttonPoll == DN) {
+			rowSel = (rowSel + 1)%Rows;
+			drawCursor();
+		}
+		else if(buttonPoll == UP) {
+			rowSel = (rowSel - 1)%Rows;
+			drawCursor();
 		}
 		
-	}
-	/*här placeras skeppen ut beroende på type, alla skepp är i sidleds ); */
-	NumShips++;
-	if (shipType == 0) {
-		playerOneBoard[rows][cols] = 1;
-		playerOneBoard[rows][cols + 1] = 1;
-		playerOneBoard[rows][cols + 2] = 1;
-		playerOneBoard[rows][cols + 3] = 1;
-		playerOneBoard[rows][cols + 4] = 1;
-		shipType++;
-	}
-	else if (shipType == 1) {
-		playerOneBoard[rows][cols] = 1;
-		playerOneBoard[rows][cols + 1] = 1;
-		playerOneBoard[rows][cols + 2] = 1;
-		playerOneBoard[rows][cols + 3] = 1;
-		shipType++;
-	}
-	else if (shipType == 2) {
-		playerOneBoard[rows][cols] = 1;
-		playerOneBoard[rows][cols + 1] = 1;
-		playerOneBoard[rows][cols + 2] = 1;
-		shipType++;
-	}
-	else if (shipType == 3) {
-		playerOneBoard[rows][cols] = 1;
-		playerOneBoard[rows][cols + 1] = 1;
-		playerOneBoard[rows][cols + 2] = 1;
-		shipType++;
-	}
-	else if (shipType == 4) {
-		playerOneBoard[rows][cols] = 1;
-		playerOneBoard[rows][cols + 1] = 1;
-	}
-	}
-	/*placera antal skäpp*/
-	for (i = 0; i < NumShips; i++) {
-		/* sålänge man inte tryckt select så ska den navigera i matrisen*/
-		while (buttonPoll != SEL)
-		{
-			/*nollar cols och rows om man går över/under sålänge*/
-			if (cols <= 10 && cols >= 0 && rows <= 10 && rows >= 0)
-			{
-				
-
-					if (buttonPoll == DN) {
-						rows++;
-					}
-				if else(buttonPoll == UP) {
-					rows--;
-				}
-				if else(buttonPoll == L) {
-					cols--;
-				}
-				if else(buttonPoll == R) {
-					cols++;
-				}
-
-
-			}
-			else
-			{
-				rows = 0;
-				cols = 0;
-			}
-			
+		//pushing left and right simultaneously generates a toggle which is used when toggling between friendly and shoot boards
+		if(buttonPoll() == L + R){
+			toggle != toggle;
 		}
+	}
+}
+
+/** Let players place ships on their boards, starting with player 1 */
+void placeShips{
+	int colSel = 0;
+	int rowSel = 0;
+	setup = true;
+	
+	//player 2
+	for(int i = 0; i < NumShips; i++){
+		
+		waitForSelect();
+		
 		NumShips++;
 		if (shipType == 0) {
-			playerTwoBoard[rows][cols] = 1;
-			playerTwoBoard[rows][cols + 1] = 1;
-			playerTwoBoard[rows][cols + 2] = 1;
-			playerTwoBoard[rows][cols + 3] = 1;
-			playerTwoBoard[rows][cols + 4] = 1;
+			playerOneBoard[rowSel][colSel] = 1;
+			playerOneBoard[rowSel][colSel + 1] = 1;
+			playerOneBoard[rowSel][colSel + 2] = 1;
+			playerOneBoard[rowSel][colSel + 3] = 1;
+			playerOneBoard[rowSel][colSel + 4] = 1;
 			shipType++;
+			
 		}
+		
 		else if (shipType == 1) {
-			playerTwoBoard[rows][cols] = 1;
-			playerTwoBoard[rows][cols + 1] = 1;
-			playerTwoBoard[rows][cols + 2] = 1;
-			playerTwoBoard[rows][cols + 3] = 1;
+			playerOneBoard[rowSel][colSel] = 1;
+			playerOneBoard[rowSel][colSel + 1] = 1;
+			playerOneBoard[rowSel][colSel + 2] = 1;
+			playerOneBoard[rowSel][colSel + 3] = 1;
 			shipType++;
 		}
+		
 		else if (shipType == 2) {
-			playerTwoBoard[rows][cols] = 1;
-			playerTwoBoard[rows][cols + 1] = 1;
-			playerTwoBoard[rows][cols + 2] = 1;
+			playerOneBoard[rowSel][colSel] = 1;
+			playerOneBoard[rowSel][colSel + 1] = 1;
+			playerOneBoard[rowSel][colSel + 2] = 1;
 			shipType++;
 		}
+		
 		else if (shipType == 3) {
-			playerTwoBoard[rows][cols] = 1;
-			playerTwoBoard[rows][cols + 1] = 1;
-			playerTwoBoard[rows][cols + 2] = 1;
+			playerOneBoard[rowSel][colSel] = 1;
+			playerOneBoard[rowSel][colSel + 1] = 1;
+			playerOneBoard[rowSel][colSel + 2] = 1;
 			shipType++;
 		}
 		else if (shipType == 4) {
-			playerTwoBoard[rows][cols] = 1;
-			playerTwoBoard[rows][cols + 1] = 1;
+			playerOneBoard[rowSel][colSel] = 1;
+			playerOneBoard[rowSel][colSel + 1] = 1;
+			shipType = 0;
+		}
+		drawBoard();
+	}
+	
+	//player 2
+	for (int i = 0; i < NumShips; i++) {
+		
+		waitForSelect();
+		
+		NumShips++;
+		if (shipType == 0) {
+			playerTwoBoard[rowSel][colSel] = 1;
+			playerTwoBoard[rowSel][colSel + 1] = 1;
+			playerTwoBoard[rowSel][colSel + 2] = 1;
+			playerTwoBoard[rowSel][colSel + 3] = 1;
+			playerTwoBoard[rowSel][colSel + 4] = 1;
+			shipType++;
+		}
+		else if (shipType == 1) {
+			playerTwoBoard[rowSel][colSel] = 1;
+			playerTwoBoard[rowSel][colSel + 1] = 1;
+			playerTwoBoard[rowSel][colSel + 2] = 1;
+			playerTwoBoard[rowSel][colSel + 3] = 1;
+			shipType++;
+		}
+		else if (shipType == 2) {
+			playerTwoBoard[rowSel][colSel] = 1;
+			playerTwoBoard[rowSel][colSel + 1] = 1;
+			playerTwoBoard[rowSel][colSel + 2] = 1;
+			shipType++;
+		}
+		else if (shipType == 3) {
+			playerTwoBoard[rowSel][colSel] = 1;
+			playerTwoBoard[rowSel][colSel + 1] = 1;
+			playerTwoBoard[rowSel][colSel + 2] = 1;
+			shipType++;
+		}
+		else if (shipType == 4) {
+			playerTwoBoard[rowSel][colSel] = 1;
+			playerTwoBoard[rowSel][colSel + 1] = 1;
 		}
 	}
-}
-void shootShips{
-	public int winner = 0;
-	public int cols = 0;
-	public int rows = 0;
-	public int playerOneHit = 0;
-	public int playerTwoHit = 0;
-	bool noWinner = true;
-	/* kör while loopen så länge ingen har vunnit*/
-while (noWinner) {
-	while (buttonPoll != SEL)
-	{
-		/*samma navigering som i placeShip metoden*/
-		if (cols <= 10 && cols >= 0 && rows <= 10 && rows >= 0) {
-			if (buttonPoll == DN) {
-				rows++;
-			}
-			if else(buttonPoll == UP) {
-				rows--;
-			}
-			if else(buttonPoll == L) {
-				cols--;
-			}
-			if else(buttonPoll == R) {
-				cols++;
-			}
-
-		}
-		else
-		{
-			rows = 0;
-			cols = 0;
-		}
-
-	}
-	/*Om spelare 1 skjuter på en plats och den platsen är markerad med ett skepp på spealer 2 så ska det vara en träff*/
-	playerOneShootBoard[rows][cols] = 1;
-	if (playerOneShotBoard[rows][cols] == 1 && playerTwoBoard[rows][cols] == 1) {
-		/*2:a betyder träffat skepp, ska synas för träffad spelare och skjutande spelare*/
-		playerOneShotBoard[rows][cols] = 2;
-		playerTwoBoard[][]
-		playerOneHits++;
-	}
-
-	while (buttonPoll != SEL)
-	{
-		if (cols <= 10 && cols >= 0 && rows <= 10 && rows >= 0) {
-			if (buttonPoll == DN) {
-				rows++;
-			}
-			if else(buttonPoll == UP) {
-				rows--;
-			}
-			if else(buttonPoll == L) {
-				cols--;
-			}
-			if else(buttonPoll == R) {
-				cols++;
-			}
-
-		}
-		else
-		{
-			rows = 0;
-			cols = 0;
-		}
-
-	}
-	playerTwoShootBoard[rows][cols] = 1;
-	if (playerTwoShotBoard[rows][cols] == 1 && playerOneBoard[rows][cols] == 1) {
-		playerTwoHits++;
-	}
-	/*totalt ska man ha 17 unika träffar för att ha sänkt alla skepp*/
-	if (playerOneHits == 17) {
-		winner = 1;
-		noWinner = false;
-	}
-	else if (playerTwoHits == 17) {
-		winner = 2;
-		noWinner = false;
-	}
-}
+	
+	setup = false;
+	
 }
 
+/** Game commences as each player is allowed to take turn shooting each others' ships
+	ShootBoard: 0 = no shot attempted; 1 = shot, but missed; 2 = shot, and hit!*/
+void shootShips(void){
+	
+	//Player one's turn
+	if(noWinner == true) {
+		waitForSelect();
+	}
 
+	if (playerTwoBoard[rowSel][colSel] == 1) {
+		playerOneShootBoard[rowSel][colSel] = 2;
+		playerOneHit++;
+		if (playerOneHit == 17) {
+			winner = 1;
+			noWinner = false;
+		}
+	}
+	else{
+		playerOneShootBoard[rowSel][colSel] = 1;
+	}
 
-
+	//Player two's turn
+	if(noWinner == true) {
+		waitForSelect();
+	}
+	
+	if (playerOneBoard[rowSel][colSel] == 1) {
+		playerTwoShootBoard[rowSel][colSel] = 2;
+		playerTwoHit++;
+		if (playerTwoHit == 17) {
+			winner = 2;
+			noWinner = false;
+		}
+	}
+	else{
+		playerTwoShootBoard[rowSel][colSel] = 1;
+	}
+}
 
 #endif /* GAME_ENGINE_H_ */
