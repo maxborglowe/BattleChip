59baf787bb845f378406cdd74e343d05e1db78a1
Juhnke057 <57365322+Juhnke057@users.noreply.github.com>
2020-05-19 14:07:04 +0200

    Update game_engine.h

--- game_engine.h
+++ game_engine.h
@@ -19,7 +19,7 @@ uint8_t DN = 1, UP = 2, L = 4, R = 8, SEL = 16;
 
 #define NumShips  5
 
-
+/* Init. knappar */
 void buttonInit()
 {
 	DDR_BUTTONS = 0x00; /* Set DDR of I/O bus A to INPUT */
@@ -58,24 +58,31 @@ void selectGameType(uint8_t game_type){
 /*Board ska vara 10x10 kommer behöva 2st
 context_save och Context_restore för att byta plan*/
 void initTwoPlayerGame(void){
+	/* spelare 1:s gamebaord, 2D matris*/
 	public int playerOneBoard[10][10];
+	/* spelare 1:s skjutbana dvs var han/hon skjuter */
 	public int playerOneShootBoard[10][10];
-
+	/* samma för spealre 2*/
 	public int playerTwoBoard[10][10];
 	public int playerTwoShootBoard[10][10]
 	
 }
 void placeShips{
+	/* shiptype är en int mellan 0-5 där siffran bestämmer vilket skäpp (längd)  */
 	int shipType = 0;
+	/* starta länst uppe till vänster i matrisen dvs 0,0 */
 	public int cols = 0;
 	public int rows = 0;
+	/* ska placera 5 skepp dvs numShips*/
 	for(i = 0; i < NumShips; i++){
+	/* stannar i while loopen så länge man inte trycker select så man kan använda alla andra knappar */
 	while (buttonPoll != SEL)
 	{
+		/* ser till att man håller sig inom matrisen, gåt till 0,0 om man går utanför*/
 		if (cols <= 10 && cols >=0 && rows <= 10 && rows >= 0)
 		{
 			
-		
+			/* navigering inom matrisen som bygger på button_poll*/
 			if (buttonPoll == DN) {
 			rows++;
 			}
@@ -98,6 +105,7 @@ void placeShips{
 		}
 		
 	}
+	/*här placeras skeppen ut beroende på type, alla skepp är i sidleds ); */
 	NumShips++;
 	if (shipType == 0) {
 		playerOneBoard[rows][cols] = 1;
@@ -204,9 +212,11 @@ void shootShips{
 	public int playerOneHit = 0;
 	public int playerTwoHit = 0;
 	bool noWinner = true;
+	/* kör while loopen så länge ingen har vunnit*/
 while (noWinner) {
 	while (buttonPoll != SEL)
 	{
+		/*samma navigering som i placeShip metoden*/
 		if (cols <= 10 && cols >= 0 && rows <= 10 && rows >= 0) {
 			if (buttonPoll == DN) {
 				rows++;
@@ -229,6 +239,7 @@ while (noWinner) {
 		}
 
 	}
+	/*Om spelare 1 skjuter på en plats och den platsen är markerad med ett skepp på spealer 2 så ska det vara en träff*/
 	playerOneShootBoard[rows][cols] = 1;
 	if (playerOneShotBoard[rows][cols] == 1 && playerTwoBoard[rows][cols] == 1) {
 		playerOneHits++;
@@ -262,7 +273,7 @@ while (noWinner) {
 	if (playerTwoShotBoard[rows][cols] == 1 && playerOneBoard[rows][cols] == 1) {
 		playerTwoHits++;
 	}
-
+	/*totalt ska man ha 17 unika träffar för att ha sänkt alla skepp*/
 	if (playerOneHits == 17) {
 		winner = 1;
 		noWinner = false;
