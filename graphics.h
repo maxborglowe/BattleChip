2540838fd7c221484d4d65b88d24f9ff4ed3461f
maxborglowe <62751158+maxborglowe@users.noreply.github.com>
2020-05-19 16:14:56 +0200

    Update graphics.h
    cursor now also shows ships that are to be placed during "setup mode"

--- graphics.h
+++ graphics.h
@@ -3,8 +3,7 @@
  *
  * Created: 2020-05-19 11:17:31
  *  Author: maxbo
- */ 
-
+ */
 
 #ifndef GRAPHICS_H_
 #define GRAPHICS_H_
@@ -16,11 +15,30 @@ uint8_t boxWidth = 25;
 uint8_t lineWidth = 1;
 uint8_t pointWidth = 10;
 
-void drawBoard(uint8_t selectBoard){
+void drawBoard(){
 	toggle = false;
-	coproc_clear_color_rgb(0, 0, 0);
-	drawBoardBoundaries(boardOrigoX, boardOrigoY, boxWidth);
+	coproc_clear_color_rgb(0, 0, 0); //clears screen of old junk
+	drawBoardBoundaries(boardOrigoX, boardOrigoY, boxWidth); //draws a border matrix in which ships are placed
+	
+	//draws shoot/friedly board depending on toggle value
+	if(toggle == false){
+		drawFriendlyBoard();
+	}
+	else if(toggle == true){
+		drawShootBoard();
+	}
 	drawCursor();
+}
+
+/** draw point on board*/
+void drawPoint(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b){
+	coproc_color_rgb(r, g, b);
+	coproc_point_size(pointWidth);
+	coproc_begin_primitive(POINT);
+	coproc_vertex2F(boardOrigoX + x * boxWidth - boxWidth/2, boardOrigoY + y * boxWidth - boxWidth/2);
+	//stop drawing primitives
+	coproc_end();
+	coproc_display();
 }
 
 void drawBoardBoundaries(uint8_t startX, uint8_t startY, uint8_t boxWidth){
@@ -45,7 +63,7 @@ void drawBoardBoundaries(uint8_t startX, uint8_t startY, uint8_t boxWidth){
 	coproc_display();
 }
 
-/** */
+/** draws the player's own placed ships on the board*/
 void drawFriendlyBoard(void){
 	for (int i = 0; i < Rows; i++)
 	{
@@ -53,16 +71,13 @@ void drawFriendlyBoard(void){
 		{
 			if (playerOneBoard[i][k] == 1)
 			{
-				coproc_color_rgb(255, 255, 255);
-				coproc_point_size(pointWidth);
-				coproc_vertex2F(boardOrigoX + i * boxWidth - boxWidth/2, boardOrigoY + k * boxWidth - boxWidth/2);
-				coproc_end();
-				coproc_display();
+				drawPoint(i, k, 255, 255, 255);
 			}
 		}
 	}
 }
 
+/** draw board that represents all the positions that the player has shot at */
 void drawShootBoard(void){
 	for (int i = 0; i < Rows; i++)
 	{
@@ -71,32 +86,63 @@ void drawShootBoard(void){
 			//draw white dot if shoot and miss
 			if (playerOneBoard[i][k] == 1)
 			{
-				coproc_color_rgb(255, 255, 255);
-				coproc_point_size(pointWidth);
-				coproc_vertex2F(boardOrigoX + i * boxWidth - boxWidth/2, boardOrigoY + k * boxWidth - boxWidth/2);
-				coproc_end();
-				coproc_display();
+				drawPoint(i, k, 255, 255, 255);
 			}
 			//draw red dot if shoot and hit
 			else if (playerOneBoard[i][k] == 2)
 			{
-				coproc_color_rgb(0, 255, 0);
-				coproc_point_size(pointWidth);
-				coproc_vertex2F(boardOrigoX + i * boxWidth - boxWidth/2, boardOrigoY + k * boxWidth - boxWidth/2);
-				coproc_end();
-				coproc_display();
+				drawPoint(i, k, 0, 255, 0);
 			}
 		}
 	}
 }
 
+/** draw ship location during ship placement */
+void drawShip(uint8_t shipType){
+	if (shipType == 0)
+	{
+		
+	}
+}
+
+/** Draws a yellow cursor dot on the board, and if in setup mode - the ships to be placed are shown and updated aswell */
 void drawCursor(){
-	coproc_color_rgb(255, 255, 0);
-	coproc_point_size(pointWidth);
-	coproc_vertex2F(boardOrigoX + rowSel * boxWidth - boxWidth/2, boardOrigoY + colSel * boxWidth - boxWidth/2);
-	//stop drawing primitives
-	coproc_end();
-	coproc_display();
+	
+	//yellow color
+	int r = 255, g = 255, b = 0;
+	
+	drawPoint(rowSel, colSel, r, g, b);
+	
+	if (setup == true)
+	{
+		if(shipType == 0){
+			drawPoint(rowSel, colSel, r, g, b);
+			drawPoint(rowSel, colSel + 1, r, g, b);
+			drawPoint(rowSel, colSel + 2, r, g, b);
+			drawPoint(rowSel, colSel + 3, r, g, b);
+			drawPoint(rowSel, colSel + 4, r, g, b);
+		}
+		else if(shipType == 1){
+			drawPoint(rowSel, colSel, r, g, b);
+			drawPoint(rowSel, colSel + 1, r, g, b);
+			drawPoint(rowSel, colSel + 2, r, g, b);
+			drawPoint(rowSel, colSel + 3, r, g, b);
+		}
+		else if(shipType == 2){
+			drawPoint(rowSel, colSel, r, g, b);
+			drawPoint(rowSel, colSel + 1, r, g, b);
+			drawPoint(rowSel, colSel + 2, r, g, b);
+		}
+		else if(shipType == 3){
+			drawPoint(rowSel, colSel, r, g, b);
+			drawPoint(rowSel, colSel + 1, r, g, b);
+			drawPoint(rowSel, colSel + 2, r, g, b);
+		}
+		else if(shipType == 4){
+			drawPoint(rowSel, colSel, r, g, b);
+			drawPoint(rowSel, colSel + 1, r, g, b);
+		}
+	}
 }
 
 #endif /* GRAPHICS_H_ */
