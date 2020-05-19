b074ada55706eb6e204ab96f5101c4f723cf8129
maxborglowe <62751158+maxborglowe@users.noreply.github.com>
2020-05-19 10:45:06 +0200

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -1,7 +1,21 @@
 #ifndef GRAPHICS_H_
 #define GRAPHICS_H_
 
+
+
+void drawBoard(){
+	
+	
+}
+
+void drawSquare(){
+	uint8_t side = 16; //length of square sides
+	coproc_begin_primitive();
+	
+}
+
 void drawShip(){
+	
 }
 
 #endif
